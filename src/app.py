849ed940da573eed68f3e9d75500f70c31a2a3fa
Namit Gupta <namitg677@gmail.com>
2025-04-17 09:01:53 +0000

    small change

--- src/app.py
+++ src/app.py
@@ -21,7 +21,7 @@ app.mount("/static", StaticFiles(directory=os.path.join(Path(__file__).parent,
 
 # In-memory activity database
 activities = {
-    "Chess Club": {
+    "Chess Clubb": {
         "description": "Learn strategies and compete in chess tournaments",
         "schedule": "Fridays, 3:30 PM - 5:00 PM",
         "max_participants": 12,
